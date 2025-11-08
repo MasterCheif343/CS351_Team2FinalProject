929de1a8cdd2955c279c274303e0230084efded9
Adam Krenek <oof799444@gmail.com>
2025-11-08 12:42:40 -0600

    Worked on game
    based off of this guide:
    https://www.youtube.com/playlist?list=PL0GUZtUkX6t6wXF0U0WAQNVYL68pYUCZv

--- Final Project/Assets/Scripts/playerMovement.cs
+++ Final Project/Assets/Scripts/playerMovement.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D rb;
+    Vector2 motionVector;
+    Animator animator;
+    [SerializeField] public float speed = 10f;
+
+    public float horizontalInput;
+    public float verticalInput;
+
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        motionVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        animator.SetFloat("horizontal", Input.GetAxisRaw("Horizontal"));
+        animator.SetFloat("vertical", Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void Move()
+    {
+       rb.velocity = motionVector * speed;
     }
 }
